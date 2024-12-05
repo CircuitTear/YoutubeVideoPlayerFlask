36f4bd984d051c9cfbcce00059a11e32b2a2912d
OG-Fishies <165400445+Egg9836@users.noreply.github.com>
2024-12-05 09:16:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
+import requests
 from pytubefix import YouTube
 import pytubefix
 
@@ -22,8 +23,8 @@ def apivideo():
     except pytubefix.exceptions.RegexMatchError:
         return "could not find match for " + str(url)
     video = video["adaptiveFormats"][0]
-
-    return redirect(video["url"])
+    videodata = requests.request("GET", video["url"], headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"}, stream=True,)
+    return Response(videodata.content, 200, mimetype="video/mp4")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
