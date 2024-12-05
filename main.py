304536d3dfe6a0357302b32459e0df587014cc4a
OG-Fishies <165400445+Egg9836@users.noreply.github.com>
2024-12-05 10:10:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response, send_file
+import requests
 from pytubefix import YouTube
 import pytubefix
 
@@ -14,15 +15,20 @@ def apivideo():
     if not url:
         return 'Missing url', 400
     try:
-        video = YouTube(url).streaming_data
+        video = YouTube(url).streams.filter(file_extension="mp4").order_by('resolution').desc().first()
     except pytubefix.exceptions.AgeRestrictedError:
         return "Age restricted"
     except pytubefix.exceptions.VideoUnavailable:
         return "Video unavailable"
     except pytubefix.exceptions.RegexMatchError:
         return "could not find match for " + str(url)
-    video = video["adaptiveFormats"][0]
-    return redirect(video["url"])
+    
+    video.download(filename="temp.mp4")
+
+    with open("temp.mp4", "rb") as f:
+        videod = f.read()
+
+    return Response(videod, 200, mimetype="video/mp4")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
